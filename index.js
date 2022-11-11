881a25a482b789b8b9990e670641d29966a141db
gwumex <42099116+gwumex@users.noreply.github.com>
2022-11-11 14:05:01 +0100

    automatically close dropdown

--- index.js
+++ index.js
@@ -5,22 +5,52 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
+    if(response.data.Error){
+        return [];
+    }
     return response.data.Search;
 };
+
+const root = document.querySelector('.autocomplete');
+    root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class = "input"></input>
+    <div class = "dropdown">
+    
+    <div class="dropdown-menu">
+    <div class="dropdown-content results">
+    </div>
+
+    </div>
+    </div>
+    `;
+
  
 const input = document.querySelector('input');
-
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
 const onInput = async (e) => {
         if(e.target.value.match("[a-zA-Z]")){
           const movies = await fetchData(e.target.value);
+          resultsWrapper.innerHTML = "";
+          dropdown.classList.add('is-active');
+
           for(let movie of movies){
-            const div = document.createElement("div");
-            div.innerHTML = `
+            const option = document.createElement('a');
+            const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+            option.classList.add("dropdown-item")
+            option.innerHTML = `
             <img src="${movie.Poster}" alt="" />
-            <h1>${movie.Title}</h1>
+           ${movie.Title}
             `;
-            document.querySelector('#target').appendChild(div);
+            resultsWrapper.appendChild(option);
           }
     }
 }
-input.addEventListener("input", debounce(onInput, 500))
+input.addEventListener("input", debounce(onInput, 1000))
+
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
+        dropdown.classList.remove("is-active")
+    }
+})
