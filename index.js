290674af7703d2b28154e76d7728a8948ea6da1c
gwumex <42099116+gwumex@users.noreply.github.com>
2022-11-11 14:15:49 +0100

    handle empty response

--- index.js
+++ index.js
@@ -32,6 +32,11 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async (e) => {
         if(e.target.value.match("[a-zA-Z]")){
           const movies = await fetchData(e.target.value);
+
+          if(!movies.length){
+            dropdown.classList.remove("is-active");
+            return;
+          }
           resultsWrapper.innerHTML = "";
           dropdown.classList.add('is-active');
 
