93d60aa3b88f4f66879afbd1fc865b044d7a5267
gwumex <42099116+gwumex@users.noreply.github.com>
2022-11-11 16:34:46 +0100

    rendered other statistics

--- index.js
+++ index.js
@@ -32,7 +32,6 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async (e) => {
     if (e.target.value.match("[a-zA-Z]")) {
         const movies = await fetchData(e.target.value);
-
         if (!movies.length) {
             dropdown.classList.remove("is-active");
             return;
@@ -75,5 +74,45 @@ const onMovieSelect = async (movie) => {
             i: movie.imdbID
         }
     });
-    console.log(response.data);
+    document.querySelector('.summary').innerHTML = movieTemplate(response.data);
+    (response.data);
+}
+
+const movieTemplate = (movieDetail) => {
+    return `
+ <article class="media">
+  <figure class="media-left">
+    <p class="image">
+      <img src="${movieDetail.Poster}" alt="">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Title}</h4>
+      <p>${movieDetail.Plot}</p>
+    </div>
+  </div>
+</article>
+<article class="notification is-primary">
+<p class="title">${movieDetail.Awards}</p>
+<p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+<p class="title">${movieDetail.BoxOffice}</p>
+<p class="subtitle">Box Office</p>
+</article>
+<article class="notification is-primary">
+<p class="title">${movieDetail.Metascore}</p>
+<p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
+<p class="title">${movieDetail.imdbRating}</p>
+<p class="subtitle">imdb Rating</p>
+</article>
+<article class="notification is-primary">
+<p class="title">${movieDetail.imdbVotes}</p>
+<p class="subtitle">imdb Votes</p>
+</article>
+ `
 }
